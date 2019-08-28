12d305a926bea9cfa3599cb681830e95a11cf42f
maxpozdniakow <53538184+maxpozdniakow@users.noreply.github.com>
2019-08-28 14:11:11 +0300

    Update setup.py

--- pycalc/setup.py
+++ pycalc/setup.py
@@ -1,14 +1,2 @@
-from setuptools import setup
-
-setup(name='pycalc',
-      version='0.1',
-      description='simple package',
-#       url='http://github.com/storborg/funniest',
-      author='Maxon',
-      author_email='asd@example.com',
-      license='MIT',
-      packages=['pycalc'],
-      zip_safe=False)
-
-
-
+if __name__ == '__main__':
+      print(0)
