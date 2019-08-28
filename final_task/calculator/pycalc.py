9afcf574fda75ffb44d3198e3a0592394af4cf8e
maxpozdniakow <53538184+maxpozdniakow@users.noreply.github.com>
2019-08-28 14:54:16 +0300

    Add files via upload

---
+++ final_task/calculator/pycalc.py
@@ -0,0 +1,15 @@
+
+import argparse
+
+def main()
+	return 0
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='simple package. nothing simpler than that.')
+    args = parser.parse_args()
+
+    print (main())
+
+
+
+
