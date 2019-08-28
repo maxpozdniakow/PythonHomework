139a9cff42bae0836f8c1a0e94da23fe04aba297
maxpozdniakow <53538184+maxpozdniakow@users.noreply.github.com>
2019-08-28 16:30:00 +0300

    Update pycalc.py

--- final_task/calculator/pycalc.py
+++ final_task/calculator/pycalc.py
@@ -2,13 +2,7 @@ import argparse
 
 
 def main():
-    return 0
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description='Pure-python command-line calculator.')
-    parser.add_argument('EXPRESSION', type=str,
-                        help='expression string to evaluate')
+    parser = argparse.ArgumentParser(description='Pure-python command-line calculator.')
+    parser.add_argument('EXPRESSION', type=str, help='expression string to evaluate')
     args = parser.parse_args()
-    print(main())
+    print(0)
