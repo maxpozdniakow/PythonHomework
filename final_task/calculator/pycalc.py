014d59f3e7d2ed171abf2865077ab7f8c1500e9b
maxpozdniakow <53538184+maxpozdniakow@users.noreply.github.com>
2019-08-28 15:52:49 +0300

    Update pycalc.py

--- final_task/calculator/pycalc.py
+++ final_task/calculator/pycalc.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='simple package. nothing simpler than that.')
     args = parser.parse_args()
 
-    print (main())
+    print (9)
 
 
 
