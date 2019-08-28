3beafe4783aa106d8fafe86282b2bde538a60a29
maxpozdniakow <53538184+maxpozdniakow@users.noreply.github.com>
2019-08-28 14:01:01 +0300

    Add files via upload

---
+++ pycalc/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='pycalc',
+      version='0.1',
+      description='simple package',
+#       url='http://github.com/storborg/funniest',
+      author='Maxon',
+      author_email='asd@example.com',
+      license='MIT',
+      packages=['pycalc'],
+      zip_safe=False)
+
+
+
