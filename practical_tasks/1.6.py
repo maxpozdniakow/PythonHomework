bc362aa85d0fc6fa345f7e70627e0d0e1aea45ed
maxpozdniakow <53538184+maxpozdniakow@users.noreply.github.com>
2019-08-29 17:06:23 +0300

    Add files via upload

---
+++ practical_tasks/1.6.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+st = 'Python is simple and effective!'
+x = st.split()
+x.sort(key=len)
+print(max(x,key=len))
+print(min(x,key=len))
+
+
+
+
+
+
+
+        
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
